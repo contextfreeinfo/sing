e2e5ae93c1d71aa841797a23bef7971c70a53d81
Tom <tom@contextfree.info>
2026-02-12 07:51:42 -0700

    Animate; show fps

--- src/main.rs
+++ src/main.rs
@@ -23,10 +23,27 @@ async fn main() -> Result<()> {
     // 3. Execute the script
     lua.load(script).exec()?;
 
+    let mut pos = Vec2::ZERO;
+    let size = Vec2::new(40.0, 40.0);
+    let mut dir = Vec2::new(1.0, 1.0);
+    let speed = 400.0;
+    let screen_size = Vec2::new(screen_width(), screen_height());
     loop {
         clear_background(BLUE);
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, WHITE);
-        draw_text("Hi there!", 20.0, 20.0, 30.0, WHITE);
+        draw_rectangle(pos.x, pos.y, size.x, size.y, WHITE);
+        let end = pos + size;
+        if pos.x < 0.0 {
+            dir.x = 1.0;
+        } else if end.x > screen_size.x {
+            dir.x = -1.0;
+        }
+        if pos.y < 0.0 {
+            dir.y = 1.0;
+        } else if end.y > screen_size.y {
+            dir.y = -1.0;
+        }
+        pos += dir * speed * get_frame_time();
+        draw_text(&format!("FPS: {}", get_fps()), 200.0, 150.0, 80.0, YELLOW);
         next_frame().await
     }
 }
