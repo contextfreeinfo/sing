48ce28864044bce99d504c924f52b0b166465569
Tom <tom@contextfree.info>
2026-02-12 06:04:40 -0700

    Add macroquad

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
+use macroquad::prelude::*;
 use mlua::{Lua, Result};
 
-fn main() -> Result<()> {
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Sing".into(),
+        fullscreen: true,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() -> Result<()> {
     // 1. Create a new Lua instance
     let lua = Lua::new();
 
@@ -13,5 +23,10 @@ fn main() -> Result<()> {
     // 3. Execute the script
     lua.load(script).exec()?;
 
-    Ok(())
+    loop {
+        clear_background(BLUE);
+        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, WHITE);
+        draw_text("Hi there!", 20.0, 20.0, 30.0, WHITE);
+        next_frame().await
+    }
 }
